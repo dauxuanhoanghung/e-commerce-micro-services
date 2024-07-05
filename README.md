e41796a507ba4557b478eebfda5c53bfe8374d32
Hung Dau <dxhh2510@gmail.com>
2024-07-05 12:15:58 +0700

    init web-app

--- README.md
+++ README.md
@@ -27,6 +27,13 @@ This's a e-commerce project. Try to implement micro-service with multi language
 
 ### Instructions
 
+0.5 After cloning
+
+```bash
+echo '#!/bin/sh' > .git/hooks/pre-commit
+echo 'cd web-app && npm run format' >> .git/hooks/pre-commit
+```
+
 1. Before developing, run commands to initialize database
 
 ```sh
--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Toaster } from '@/components/ui/sonner';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Ecommerce Microservices",
-  description: "This is e-commerce web app built with microservices",
+  title: 'Ecommerce Microservices',
+  description: 'This is e-commerce web app built with microservices',
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
